4b1212e586f20a0496697ffbf496c81f7f93d9fb
Loco1982 <loco@DESKTOP-OCBL1GQ>
2023-05-25 15:07:25 -0300

    refactorized global variables

--- pi_murders.ex
+++ pi_murders.ex
@@ -1,9 +1,10 @@
 defmodule PiMurders do
 
- def months_amount(digit_amount) do
-  set = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6, 2, 6, 4, 3, 3,
+  @pi [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6, 2, 6, 4, 3, 3,
   8, 3, 2, 7, 9, 5, 0, 2, 8, 8, 4, 1, 9, 7]
-  pi_digits = Enum.take(set, digit_amount)
+
+ def months_amount(digit_amount) do
+  pi_digits = Enum.take(@pi, digit_amount)
   months = Enum.reduce(pi_digits, fn x, acc -> x + acc end)
   pi_digit = List.pop_at(pi_digits, -1)
   {digit, _} = pi_digit
@@ -11,9 +12,7 @@ defmodule PiMurders do
  end
 
  def accumulated_assasination_months(digit) do
-  set = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6, 2, 6, 4, 3, 3,
-  8, 3, 2, 7, 9, 5, 0, 2, 8, 8, 4, 1, 9, 7]
-  accumulated = Enum.reduce(Enum.take(set, digit), fn x, acc -> x + acc end)
+  accumulated = Enum.reduce(Enum.take(@pi, digit), fn x, acc -> x + acc end)
   "Han pasado #{accumulated} meses de asesinatos"
  end
 end
