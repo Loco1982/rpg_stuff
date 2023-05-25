12d7f764b1bb4ed52c3e47af66aa3ec368e13585
Loco1982 <loco@DESKTOP-OCBL1GQ>
2023-05-25 14:57:02 -0300

    new module to calculate pi_murders

---
+++ pi_murders.ex
@@ -0,0 +1,12 @@
+defmodule PiMurders do
+
+ def months_amount(digit_amount) do
+  set = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6, 2, 6, 4, 3, 3,
+  8, 3, 2, 7, 9, 5, 0, 2, 8, 8, 4, 1, 9, 7]
+  pi_digits = Enum.take(set, digit_amount)
+  months = Enum.reduce(pi_digits, fn x, acc -> x + acc end)
+  pi_digit = List.pop_at(pi_digits, -1)
+  {digit, _} = pi_digit
+  "El próximo asesinato será en #{months} meses y serán #{digit} asesinatos"
+ end
+end
